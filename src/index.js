40a21eac44ff59b5e6b3ffaa24440934b902fc71
FarisanSN <farisansafrialnafis@gmail.com>
2022-04-26 12:52:51 +0700

    Init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import SignPDF from "./SignPDF.js";
+import fs from "node:fs";
+import path from "node:path";
+
+async function main() {
+  const pdfBuffer = new SignPDF(
+    path.resolve('assets/minions.pdf'),
+    path.resolve('assets/Farisan.p12')
+  );
+
+  const signedDocs = await pdfBuffer.signPDF();
+  const randomNumber = Math.floor(Math.random() * 5000);
+  const pdfFileName = `./exports/exported_file_${randomNumber}.pdf`;
+
+  fs.writeFileSync(pdfFileName, signedDocs);
+  console.log(`New Signed PDF created called: ${pdfFileName}`);
+}
+
+main();
